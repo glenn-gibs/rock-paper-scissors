c12aeb6eabaf6cce6f93f0c106258db6943921e4
Glenn Gibs <glenn.gibs@hva.nl>
2021-09-07 12:12:29 +0200

    Add project files.

---
+++ rock-paper-scissors/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace rock_paper_scissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            Console.ReadKey();
+        }
+    }
+}
